aab047df846edd11ad29993244b0de0644fcb725
Poloju Rishith <polojurishith@gmail.com>
2018-05-20 16:59:14 +0530

    java application

---
+++ src/Data.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Poloj
+ */
+public class Data {
+    public static boolean occupied[]=new boolean[]{false,false,false,false,false,false,false,false,false,false};
+    public static String driverName[]=new String[10];
+    public static String vehicleNumber[]=new String[10];
+    public static String intime[]=new String[10];
+}
